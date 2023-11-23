4a26d1aa2fad7b0f037a3f756da292b90f1f48c1
jpope6 <imthepope@csu.fullerton.edu>
2023-11-23 14:02:33 -0800

    Added a switch to switch between home and away stats

--- src/components/Accordion/AccordianItem.jsx
+++ src/components/Accordion/AccordianItem.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import Spinner from "../Spinner/Spinner";
 import { teamLogos } from "../../images/logos";
+import Switch from "react-switch";
 
 
 import "./Accordion.css"
@@ -16,6 +17,14 @@ function AccordianItem({ gameData, teamData, playerData }) {
 
     function handleItemClicked() {
         setExpanded((expanded) => !expanded);
+    }
+
+    const handleSwitchClick = () => {
+        if (selectedTeam === 'home') {
+            setSelectedTeam('away');
+        } else {
+            setSelectedTeam('home');
+        }
     }
 
 
@@ -56,15 +65,44 @@ function AccordianItem({ gameData, teamData, playerData }) {
             <div className={`accordion-description ${expanded ? "expand-accordion-item" : ""}`}>
                 {playerData ? (
                     <div>
-                        <h3>Home Team Player Stats</h3>
-                        <ul>
-                            {playerData.home.map((player, index) => (
+                        <div className="description-header">
+                            <h3>
+                                {`${selectedTeam === 'home'
+                                    ?
+                                    teamData[gameData.HOME_TEAM_ID].NICKNAME
+                                    :
+                                    teamData[gameData.VISITOR_TEAM_ID].NICKNAME}`} Stats
+                            </h3>
+                            <Switch
+                                checked={selectedTeam === 'home' ? true : false}
+                                onChange={handleSwitchClick}
+                                onColor="#86d3ff"
+                                onHandleColor="#2693e6"
+                                handleDiameter={30}
+                                uncheckedIcon={false}
+                                checkedIcon={false}
+                                boxShadow="0px 1px 5px rgba(0, 0, 0, 0.6)"
+                                activeBoxShadow="0px 0px 1px 10px rgba(0, 0, 0, 0.2)"
+                                height={20}
+                                width={48}
+                                className="react-switch"
+                                id="material-switch"
+                            />
+                        </div>
+                        {selectedTeam === 'home'
+                            ? playerData.home.map((player, index) => (
                                 <div key={index}>
                                     <p>{player.PLAYER_NAME}</p>
                                     <p>MIN: {player.MIN}, PTS: {player.PTS}, AST: {player.AST}, REB: {player.REB}, BLK: {player.BLK}, STL: {player.STL}, TO: {player.TO}, PLUS_MINUS: {player.PLUS_MINUS}</p>
                                 </div>
-                            ))}
-                        </ul>
+                            ))
+                            : playerData.visitor.map((player, index) => (
+                                <div key={index}>
+                                    <p>{player.PLAYER_NAME}</p>
+                                    <p>MIN: {player.MIN}, PTS: {player.PTS}, AST: {player.AST}, REB: {player.REB}, BLK: {player.BLK}, STL: {player.STL}, TO: {player.TO}, PLUS_MINUS: {player.PLUS_MINUS}</p>
+                                </div>
+                            ))
+                        }
                     </div>
                 ) : (
                     <div className="spinner-container">
